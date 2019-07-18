447a7a9080c183efb4bc539c554e0b29bcff0b04
201402407 <banner4@cnu.ac.kr>
2019-07-18 20:19:20 +0900

    Splash 화면 추가

---
+++ app/src/main/java/apps/user/jaksimforever/SplashActivity.kt
@@ -0,0 +1,39 @@
+package apps.user.jaksimforever
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash.*
+
+/*
+ Splash 화면 액티비티.
+ GlideModule 사용
+ - Kotlin
+ - 이해원
+ */
+class SplashActivity : AppCompatActivity() {
+    private var mHandler: Handler? = null
+    private val SPLASH_TIME: Long = 4000
+    // 다음 화면으로 넘어가기 위한 Runnable 변수.
+    val mRunnable: Runnable = Runnable {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Glide에 스플래쉬 넣기
+        Glide.with(this)
+            .load(R.drawable.splash)
+            .into(splashView)
+
+        // Handler 초기화
+        mHandler = Handler()
+        mHandler!!.postDelayed(mRunnable, SPLASH_TIME)  // !!이면 Non-Null
+
+    }
+}
